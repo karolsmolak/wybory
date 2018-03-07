d8198e1fe3b1db61a8ab0abd811630d892e19baa
Karol <karol_smolak@wp.pl>
2018-03-07 11:07:20 +0100

    cosmetic changes, added district info

--- data/get_data.py
+++ data/get_data.py
@@ -1,41 +1,43 @@
-#this script fetches data from official website, converts xls to csv and merges them together
+# this script fetches data from official website, converts xls to csv and merges them together
 import urllib.request
 import pandas as pd
 import os
 
-def get_id(i):
-    return "{:02}".format(i)
 
-def get_url(id):
-    return "http://prezydent2000.pkw.gov.pl/gminy/obwody/obw" + id + ".xls"
+def get_district_id(district_no):
+    return "{:02}".format(district_no)
 
-def get_file_path(id):
-    return "obw" + id;
 
-def get_xls_sheet(id):
-    return "gl" + id
+def get_url(district_id):
+    return "http://prezydent2000.pkw.gov.pl/gminy/obwody/obw" + district_id + ".xls"
 
-def get_progress_bar(i, size):
-    progress = round(30 * i / size)
+
+def get_file_path(district_id):
+    return "obw" + district_id
+
+
+def get_xls_sheet(district_id):
+    return "gl" + district_id
+
+
+def get_progress_bar(iteration, max_iterations):
+    progress = round(30 * iteration / max_iterations)
     return "[" + "#" * progress + " " * (30 - progress) + "]"
 
-fout = open("data.csv", "a")
+
+output_file = open("data.csv", "a")
 print("Fetching data...")
 for i in range(1, 69):
-    id = get_id(i)
-    path = get_file_path(id)
-    urllib.request.urlretrieve(get_url(id), path + ".xls")
-    xls_file = pd.read_excel(path + ".xls", get_xls_sheet(id)).rename(columns=lambda x: x.strip().replace("\n", " "))
-    if i == 1:
-        xls_file.to_csv(path + ".csv", encoding='utf-8', index=False)
-    else:
-        xls_file.to_csv(path + ".csv", encoding='utf-8', index=False, header=False)
+    district_id = get_district_id(i)
+    path = get_file_path(district_id)
+    urllib.request.urlretrieve(get_url(district_id), path + ".xls")
+    xls_file = pd.read_excel(path + ".xls", get_xls_sheet(district_id)).rename(
+        columns=lambda x: x.strip().replace("\n", " "))
+    xls_file.to_csv(path + ".csv", encoding='utf-8', index=False, header=True if i == 1 else False)
     os.remove(path + ".xls")
-    f = open(path + ".csv")
-    for line in f:
-        fout.write(line)
-    f.close()
+    for line in open(path + ".csv"):
+        output_file.write(line)
     os.remove(path + ".csv")
-    print("\r" + get_progress_bar(i, 68), end = "", flush=True)
-fout.close()
+    print("\r" + get_progress_bar(i, 68), end="", flush=True)
+output_file.close()
 print("\rFinished!")
